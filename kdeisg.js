f2136369f2d441808e4fc9f71f607a7b05f4f123
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-15 10:09:11 +0900

    Add files via upload

---
+++ kdeisg.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/Lineage2M/20241106/intro_v5.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
